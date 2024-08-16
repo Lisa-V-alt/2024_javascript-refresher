46a3190a81bb6be510c92ffcd8edbf7c49a16937
lisa-v-alt <uhm.lisav@gmail.com>
2024-08-16 06:01:37 +0000

    added import and export commands and values

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,1 +1,23 @@
-import
+import { apiKey } from "./util.js"; //*
+
+//displays apiKey from util file in browser console
+console.log(apiKey);
+
+//*
+////alternative1:
+////when import uses default
+//import apiKey from "./util.js";
+
+////alternative2:
+////when importing multiple files
+//import { apiKey, abc } from ".util/js";
+
+////alternative3:
+////when importing multiple files as one huge group
+//import * as util from "util.js";
+//console.log(util.apiKey);
+
+////alternative4:
+////when importing multiple files but asigning aliases to them
+//import { apiKey, abc as content } from ".util/js";
+//console.log(content);
--- assets/scripts/util.js
+++ assets/scripts/util.js
@@ -1,1 +1,12 @@
-export
+export let apiKey = "92738Bsdf_encrypt10n";
+
+////alternative1:
+//export default "92738Bsdf_encrypt10n";
+////when 'default' is used, app.js must not have curly braces for importing
+////when using 'default', can only have one default. used when only one single function over and over
+
+////alternative2:
+////when exporting multiples
+//export default "92738Bsdf_encrypt10n"; //(can mix default with other multiples)
+//export let apiKey = "92738Bsdf_encrypt10n";
+//export let abc = "abc";
